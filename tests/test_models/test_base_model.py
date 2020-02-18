7c724778db771149621d2cd8890e215b5fb534e2
Emna <bhmemna7@gmail.com>
2020-02-18 12:07:06 +0000

    create

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+import unittest
+from models.base_model import BaseModel
+
+
+class testbase(unittest.TestCase):
+    def test_id(self):
+        b = BaseModel()
+        b1 = BaseModel()
+        self.assertNotEqual(b.id, b1.id)
