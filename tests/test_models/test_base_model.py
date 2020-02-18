aa3a04ffc30f7b94a7bfde4b7ad25230defbd0d7
Emna <bhmemna7@gmail.com>
2020-02-18 12:12:45 +0000

    create

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -4,6 +4,10 @@ from models.base_model import BaseModel
 
 
 class testbase(unittest.TestCase):
+
+    def test_id_is_public_str(self):
+        self.assertEqual(str, type(BaseModel().id))
+
     def test_id(self):
         b = BaseModel()
         b1 = BaseModel()
