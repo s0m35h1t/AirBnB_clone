ee1980d78ffa33ae163272c0f1437a4b812f9eae
Emna <bhmemna7@gmail.com>
2020-02-19 16:53:07 +0000

    create

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -3,10 +3,24 @@
 Define: Review class unitests
 """
 import unittest
-
+import models
+from datetime import datetime
 from models.review import Review
 
 
 class TestReviewMethods(unittest.TestCase):
 
-    pass
+    def test_no_args_instantiates(self):
+        self.assertEqual(Review, type(Review()))
+
+    def test_new_instance_stored_in_objects(self):
+        self.assertIn(Review(), models.storage.all().values())
+
+    def test_id_is_str(self):
+        self.assertEqual(str, type(Review().id))
+
+    def test_created_at_is_datetime(self):
+        self.assertEqual(datetime, type(Review().created_at))
+
+    def test_updated_at_is_datetime(self):
+        self.assertEqual(datetime, type(Review().updated_at))
