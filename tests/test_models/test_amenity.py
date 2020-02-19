9549f71e5a26b3d9f3a3ea10db66d274097930bb
Emna <bhmemna7@gmail.com>
2020-02-19 19:48:02 +0000

    create

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -3,10 +3,35 @@
 Define: Amenity class unitests
 """
 import unittest
-
+import models
+from datetime import datetime
 from models.amenity import Amenity
 
 
 class TestAmenityMethods(unittest.TestCase):
 
-    pass
+    def test_no_args_instantiates(self):
+        self.assertEqual(Amenity, type(Amenity()))
+
+    def test_new_instance_stored_in_objects(self):
+        self.assertIn(Amenity(), models.storage.all().values())
+
+    def test_id_is_public_str(self):
+        self.assertEqual(str, type(Amenity().id))
+
+    def test_created_at_is_public_datetime(self):
+        self.assertEqual(datetime, type(Amenity().created_at))
+
+    def test_updated_at_is_public_datetime(self):
+        self.assertEqual(datetime, type(Amenity().updated_at))
+
+    def test_name_is_public_class_attribute(self):
+        am = Amenity()
+        self.assertEqual(str, type(Amenity.name))
+        self.assertIn("name", dir(Amenity()))
+        self.assertNotIn("name", am.__dict__)
+
+    def test_two_amenities_unique_ids(self):
+        am = Amenity()
+        am1 = Amenity()
+        self.assertNotEqual(am.id, am1.id)
