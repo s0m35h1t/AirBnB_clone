f18b13e837ac469444bf294da22b210183d71b4d
Emna <bhmemna7@gmail.com>
2020-02-19 16:58:37 +0000

    create

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -16,9 +16,16 @@ class TestBaseModelMethods(unittest.TestCase):
         b = BaseModel()
         b1 = BaseModel()
         self.assertNotEqual(b.id, b1.id)
+
     def test_id_is_str(self):
-        self.assertEqual(str, type(BaseModel().id))    
+        self.assertEqual(str, type(BaseModel().id))
+
     def test_created_at_datetime(self):
         self.assertEqual(datetime, type(BaseModel().created_at))
+
     def test_updated_at_datetime(self):
         self.assertEqual(datetime, type(BaseModel().updated_at))
+
+
+if __name__ == "__main__":
+    unittest.main()
