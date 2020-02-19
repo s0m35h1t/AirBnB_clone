1846b70ba8d7e5c2de038bcf6a179579ca8dfbad
Emna <bhmemna7@gmail.com>
2020-02-19 19:39:28 +0000

    create

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -9,4 +9,17 @@ from models.place import Place
 
 class TestPlaceMethods(unittest.TestCase):
 
-    pass
+    def test_no_args_instantiates(self):
+        self.assertEqual(Place, type(Place()))
+
+    def test_new_instance_stored_in_objects(self):
+        self.assertIn(Place(), models.storage.all().values())
+
+    def test_id_str(self):
+        self.assertEqual(str, type(Place().id))
+
+    def test_created_at_datetime(self):
+        self.assertEqual(datetime, type(Place().created_at))
+
+    def test_updated_at_datetime(self):
+        self.assertEqual(datetime, type(Place().updated_at))
